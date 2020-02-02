3c9f5a56807c47bda36c2138f76dbeda75277886
Kevin Henderson <kevin.d.hende@gmail.com>
2020-02-02 13:55:52 -0600

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import requests
 from tkinter import font
+
 HEIGHT = 500
 WIDTH = 600
 
@@ -12,7 +13,7 @@ def format_response(weather):
 
         final_str = 'City: %s \nConditions: %s \nTemperature (F): %s' % (name, desc, temp)
     except:
-        final_str = 'There was a problem retrieving the weather data.'
+        final_str = 'There was a problem\n retrieving the weather data.'
     return final_str
 
 def get_weather(city):
@@ -31,17 +32,17 @@ window = tk.Tk()
 canvas = tk.Canvas(window, height=HEIGHT, width=WIDTH, bg='white')
 canvas.pack()
 
-background_image = tk.PhotoImage(file='clouds.png')
+background_image = tk.PhotoImage(file='./img/clouds.png')
 background_label = tk.Label(window, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
 frame = tk.Frame(window, bg='#5b8c85', bd=10)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
-entry = tk.Entry(frame, font=40)
+entry = tk.Entry(frame)
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="Get Weather!", font=40, command=lambda: get_weather(entry.get()), fg='#5b8c85')
+button = tk.Button(frame, text="Get Weather!", font=('Helvetica', 12), command=lambda: get_weather(entry.get()), fg='#5b8c85')
 button.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
 
 lower_frame = tk.Frame(window, bg='#5b8c85', bd=10)
